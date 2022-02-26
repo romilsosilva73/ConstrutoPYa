8a2eaa0218a03097c2a1e7b8a649990af31b538f
Romilso <romilsosilva7337@gmail.com>
2022-02-26 19:27:00 -0400

    Test 22 - Get users from api - https://reqres.in/

--- usuario.py
+++ usuario.py
@@ -1,3 +1,5 @@
+# Press Shift+F10 to execute it or replace it with your code.
+
 # 1 - imports - bibliotecas
 
 import pytest
@@ -5,28 +7,29 @@ import pytest
 import csv
 
 import requests
+from requests import HTTPError
 
 teste_dados_novos_usuarios = [
-    ('1', 'Juca', 'Pirama', 'juca@iterasys.com.br'),       #usuário 1
-    ('2', 'Agatha', 'Christie', 'agatha@iterasys.com.br')  #usuário 2
+    (1, 'Juca', 'Pirama', 'juca@iterasys.com.br'),       #usuário 1
+    (2, 'Agatha', 'Christie', 'agatha@iterasys.com.br')  #usuário 2
 ]
 
 
 teste_dados_usuarios_atuais = [
-    ('1', 'George', 'Bluth', 'george.bluth@reqres.in'), #usuário 1
-    ('2', 'Janet', 'Weaver', 'janet.weaver@reqres.in')  #usuário 2
+    (1, 'George', 'Bluth', 'george.bluth@reqres.in', 'https://reqres.in/img/faces/1-image.jpg', 'https://reqres.in/#support-heading', 'To keep ReqRes free, contributions towards server costs are appreciated!'), #usuário 1
+    (2, 'Janet', 'Weaver', 'janet.weaver@reqres.in', 'https://reqres.in/img/faces/2-image.jpg', 'https://reqres.in/#support-heading', 'To keep ReqRes free, contributions towards server costs are appreciated!') #usuário 2
 ]
 
-# CRUD em aplicaçõe
+# CRUD em aplicações
 
 #CREATE          - Post     Incluir / Publicar
 #REACH/RESEARCH  - Get      Consultar / Pagar
 # UPDATE         - Put      Atualizar
 #DELETE          - Delete   Excluir
 
-@pytest.mark.parametrize('id, nome, sobrenome, email', teste_dados_usuarios_atuais)
+@pytest.mark.parametrize('id, nome, sobrenome, email, avatar, url, text', teste_dados_usuarios_atuais)
 
-def testar_dados_usuarios(id, nome, sobrenome, email): # função que testa algo
+def testar_dados_usuarios(id, nome, sobrenome, email, avatar, url, text): # função que testa algo
     try:
         response = requests.get(f'https://reqres.in/api/users/{id}')
         jsonResponse = response.json()
@@ -34,16 +37,28 @@ def testar_dados_usuarios(id, nome, sobrenome, email): # função que testa algo
         nome_obtido = jsonResponse['data']['first_name']
         sobrenome_obtido = jsonResponse['data']['last_name']
         email_obtido = jsonResponse['data']['email']
+        avatar_obtido = jsonResponse['data']['avatar']
+        url_obtido = jsonResponse['support']['url']
+        text_obtido = jsonResponse['support']['text']
 
-        print(f'Id : {id_obtido} - Nome : {nome_obtido} - Sobre nome :  {sobrenome_obtido} - Email : {email_obtido}')
+        #print(f'Id : {id_obtido} - Nome : {nome_obtido} - Sobre nome :  {sobrenome_obtido} - Email : {email_obtido} - Avatar : {avatar_obtido}  - URL : {url_obtido} - Texto : {text_obtido}')
+
+        #print(f'Id : {id_obtido} - Nome : {nome_obtido} - Sobre nome :  {sobrenome_obtido} - Email : {email_obtido} - Avatar : {avatar_obtido}  - URL : {url_obtido} - Texto : {text_obtido}')
+
+        print(f' \n Id : {id_obtido} \n  - Nome :  {nome_obtido} \n  - Sobre nome :  {sobrenome_obtido} \n  - Email :   {email_obtido} \n  - Avatar :    {avatar_obtido}  \n  - URL :   {url_obtido} \n - Texto :   {text_obtido} \n ')
 
         assert id_obtido == id
         assert nome_obtido == nome
         assert sobrenome_obtido == sobrenome
         assert email_obtido == email
+        assert avatar_obtido == avatar
+        assert url_obtido == url
+        assert text_obtido == text
 
-    except:
-        print('Falta fazer')
+    except HTTPError as http_fail: # para o ISTQB, descobriu rodando é falha
+        print(f'Ocorre um erro de HTTP : {http_fail}')
+    except Exception as fail: # Qualquer exceção será tratada a seguir
+        print(f'Falha inesperada ocorreu : {fail}')
 
     # 2 -  class - classe
 
